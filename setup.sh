f0e7fee4a6eba8b119b0daa633097ea3fd5bf1f5
MichalSarniewicz <MichalSarniewicz@users.noreply.github.com>
2025-11-15 17:03:21 +0200

    feat: add config server repositories to setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ git clone https://github.com/user/skydive-forecast-gateway.git
 git clone https://github.com/user/skydive-forecast-user-service.git
 git clone https://github.com/user/skydive-forecast-analysis-service.git
 git clone https://github.com/user/skydive-forecast-location-service.git
+git clone https://github.com/user/skydive-forecast-config-server.git
+git clone https://github.com/user/skydive-forecast-svc-config.git
 
 echo "Setup complete"
 echo "Run: docker-compose up --build"
